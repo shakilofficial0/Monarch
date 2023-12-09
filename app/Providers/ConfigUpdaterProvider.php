94a2bf09b75577a50645be90379765ad20e114cc
Shakil Ahmed <shakilofficial0@gmail.com>
2023-12-09 19:03:51 +0600

    pushing to avoid error

--- app/Providers/ConfigUpdaterProvider.php
+++ app/Providers/ConfigUpdaterProvider.php
@@ -21,17 +21,22 @@ class ConfigUpdaterProvider extends ServiceProvider
      */
     public function boot(): void
     {   
-        $dataTable = DB::table('settings')->pluck('value', 'name')->toArray();
+        try {
+            $dataTable = DB::table('settings')->pluck('value', 'name')->toArray();
+        } catch (\Exception $e) {
+            
+        }
+        
         config([
-            "variables.templateName" => $dataTable['siteName'],
-            "variables.templateSuffix" => $dataTable['suffix'],
-            "variables.templateDescription" => $dataTable['templateDescription'],
-            "variables.support_mail" => $dataTable['support_mail'],
-            "variables.url" => $dataTable['url'],
-            "variables.templateKeyword" => $dataTable['templateKeyword'],
-            "variables.facebookUrl" => $dataTable['facebookUrl'],
-            "variables.twitterUrl" => $dataTable['twitterUrl'],
-            "variables.instagramUrl" => $dataTable['instagramUrl'],
+            "variables.templateName" => empty($dataTable['siteName']) ? config('variables.templateName') : $dataTable['siteName'],
+            "variables.templateSuffix" => empty($dataTable['suffix']) ? config('variables.templateSuffix') : $dataTable['suffix'],
+            "variables.templateDescription" => empty($dataTable['templateDescription']) ? config('variables.templateDescription') : $dataTable['templateDescription'],
+            "variables.support_mail" => empty($dataTable['support_mail']) ? config('variables.support_mail') : $dataTable['support_mail'],
+            "variables.url" => empty($dataTable['url']) ? config('variables.url') : $dataTable['url'],
+            "variables.templateKeyword" => empty($dataTable['templateKeyword']) ? config('variables.templateKeyword') : $dataTable['templateKeyword'],
+            "variables.facebookUrl" => empty($dataTable['facebookUrl']) ? config('variables.facebookUrl') : $dataTable['facebookUrl'],
+            "variables.twitterUrl" => empty($dataTable['twitterUrl']) ? config('variables.twitterUrl') : $dataTable['twitterUrl'],
+            "variables.instagramUrl" => empty($dataTable['instagramUrl']) ? config('variables.instagramUrl') : $dataTable['instagramUrl'],
         ]);
     }
 }
